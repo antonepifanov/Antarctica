af829c99cb7736f0a858a21e6777ebb5db27b7f6
antonepifanov <antonepifanov87@gmail.com>
2022-10-24 23:52:51 +0300

    Добавляет сборку

---
+++ source/js/main.js
@@ -0,0 +1,18 @@
+import {iosVhFix} from './utils/ios-vh-fix';
+import {initModals} from './modules/modals/init-modals';
+import {initPhoneMask} from './modules/init-phone-mask';
+import {initValidation} from './modules/init-validation';
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Utils
+  iosVhFix();
+
+  // Modules
+  initModals();
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initPhoneMask();
+    initValidation();
+  });
+});
